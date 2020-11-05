813c92c911db56dcb5586d6a3aefa63d7334f27c
Daniel Varela Menéndez <73996242+DanielVarelaMenendez@users.noreply.github.com>
2020-11-05 13:52:04 +0100

    Delete communications.py

--- Jetson_Nano/communications.py
+++
@@ -1,8 +0,0 @@
-import serial
-def enviar(comando,ser): #función enviar , recibe el caracter a enviar (comando) y el
-    objeto serial (ser) que puede hacer referencia tanto al mega como al esp32
-    ser.write(bytes(comando,'utf-8'))#escribe en el serial el comando
-    print('enviado',comando)
-    while (not ser.read().decode('utf-8')=='F'): #mientras no reciba una 'F' no sale de este bucle
-        print('Ejecutando accion')
-    print('F')
